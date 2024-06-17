8355f52854886a2f310760a125ba36c2baad8d3d
Guen Prawiroatmodjo <4041805+guenp@users.noreply.github.com>
2024-06-16 23:16:19 -0700

    import from collector

--- src/api_tracer/__init__.py
+++ src/api_tracer/__init__.py
@@ -1,3 +1,3 @@
 from api_tracer.path_finder import install
 from api_tracer.span import span
-from api_tracer.span_exporter.jaeger import start_span_processor
+from api_tracer.span_exporter.collector import start_span_processor
