0c6808764f737331c7fb932b82b3182a0c615f57
Guen Prawiroatmodjo <4041805+guenp@users.noreply.github.com>
2025-05-14 16:52:28 -0700

    use cookie

--- src/api_tracer/__init__.py
+++ src/api_tracer/__init__.py
@@ -1,7 +1,18 @@
+"""
+Copyright (c) 2025 Guen Prawiroatmodjo. All rights reserved.
+
+api-tracer: API tracer to gather usage telemetry for Python libraries
+"""
+
+from __future__ import annotations
+
 from api_tracer.path_finder import install
 from api_tracer.span import span
 
+from ._version import version as __version__
+
 __all__ = [
+    "__version__",
     "install",
     "span",
 ]
